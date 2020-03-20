842915d57b0c8c516e8c3217f5ef8207a0c9cf85
George Chen <georgechen1338@gmail.com>
2020-03-20 21:38:31 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,5 +11,7 @@ def print_first_directors_movie_titles
   directors_index = 0
   
   while directors_index < directors_database[0][:movies].length do
-  print directors_database[0][:movies]
+    print directors_database[0][:movies][:title]
+    directors_index += 1
+  end
 end
